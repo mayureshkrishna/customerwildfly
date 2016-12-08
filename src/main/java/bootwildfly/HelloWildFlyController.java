045c14908830b4cc8f767c29fd85da7b9aea7223
Krishna, Mayuresh (CCI-Atlanta) <Mayuresh.Krishna@cox.com>
2016-12-08 16:28:39 -0500

    testing

--- src/main/java/bootwildfly/HelloWildFlyController.java
+++ src/main/java/bootwildfly/HelloWildFlyController.java
@@ -9,6 +9,6 @@ public class HelloWildFlyController {
 
     @RequestMapping("hello")
     public String sayHello(){
-        return ("Hello, SpringBoot on Wildfly - With Updates");
+        return ("Hello, SpringBoot on Wildfly - Newer Updates 2.0");
     }
 }
